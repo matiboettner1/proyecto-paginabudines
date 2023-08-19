727c6346a8cdff3fff65c1df3747466e7a7cf86c
“matiboettner1” <matiasboettner2004@gmail.com>
2023-08-18 21:48:33 -0300

    cambia todo el index por uno renovado

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-//let nombreUsuario = prompt('Como te llamas?')
+const carousel = document.querySelector('.carousel');
+const slides = document.querySelectorAll('.slide');
 
-//alert('Hola ' + nombreUsuario + ', espero que te guste mi pagina web.')
+let currentIndex = 0;
+
+function showSlide(index) {
+  if (index < 0) {
+    currentIndex = slides.length - 1;
+  } else if (index >= slides.length) {
+    currentIndex = 0;
+  }
+
+  const offset = -currentIndex * 100;
+  carousel.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(() => {
+  currentIndex++;
+  showSlide(currentIndex);
+}, 3000);
+
+showSlide(currentIndex);
